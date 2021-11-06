b461828290cbd456545063d1f844485a8223275c
Alexey Kavin <smillerlenatderirinc@gmail.com>
2021-11-06 21:28:55 +0300

    [ADD] Ending manager

---
+++ Assets/Project/Scripts/EndingManagment/EndingManager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using Common.Components;
+using Common.Components.Interfaces;
+using Common.SceneManagement.ScriptableObjects;
+using Common.Tools;
+using Common.UI;
+using Services.SceneLoad;
+using UI;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace EndingManagment
+{
+    public class EndingManager : Singleton<EndingManager>, ICoroutineRunner
+    {
+        [SerializeField] private bool debugInfo = false;
+        [SerializeField] private float timerInSeconds = 6 * 60;
+        [SerializeField] private Scene scene;
+        [SerializeField] private PlayableDirector _director;
+        private Timer _endingTimer;
+
+        protected override void BeforeRegister()
+        {
+            SetSettings(true, true);
+        }
+
+        protected override void AfterRegister()
+        {
+            _endingTimer = new Timer(this, timerInSeconds, OnTimerEnd, debugInfo: debugInfo, name: $"{name}_endingTimer");
+        }
+
+        private void Start()
+        {
+            _endingTimer.Play();
+        }
+
+        private void OnTimerEnd()
+        {
+            print("end!");
+            _director.Play();
+        }
+        
+        public void HideGameView()
+        {
+            MainCanvas.Instance?.panel.SetActive(true);
+        }
+        
+        public void EndGame()
+        {
+            SceneService.sceneProvider.Load(scene);
+            CursorLocker.SetLocked(false);
+        }
+    }
+}
---
+++ Assets/Project/Scripts/UI/MainCanvas.cs
@@ -0,0 +1,16 @@
+﻿using Common.Components;
+using Services.SceneLoad;
+using UnityEngine;
+
+namespace UI
+{
+    public class MainCanvas : Singleton<MainCanvas>
+    {
+        public GameObject panel;
+
+        protected override void BeforeRegister()
+        {
+            SetSettings(true, true);
+        }
+    }
+}
